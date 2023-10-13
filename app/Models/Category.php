ddd77279ff7cbd4e2037015c50ea1f685fa12633
Med Faji <meedfaji@gmail.com>
2023-10-13 16:12:35 +0100

    making product, category models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable= ['name', 'parent_id'];
+
+    public function products() {
+        return $this->belongsToMany(Product::class, 'product_categories');
+    }
+
+    public function children() {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent() {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
 }

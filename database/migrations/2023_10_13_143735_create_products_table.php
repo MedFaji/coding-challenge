e86a430c724cc842f41147bb334ebdb44a89303f
Med Faji <meedfaji@gmail.com>
2023-10-13 16:03:44 +0100

    migrating database

--- database/migrations/2023_10_13_143735_create_products_table.php
+++ database/migrations/2023_10_13_143735_create_products_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->string('name');
+            $table->string('description');
+            $table->float('price');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2023_10_13_143750_create_categories_table.php
+++ database/migrations/2023_10_13_143750_create_categories_table.php
@@ -12,9 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+                    $table->id('id');
+                    $table->string('name');
+                    $table->unsignedBigInteger('parent_id')->nullable();
+                    $table->foreign('parent_id')->references('id')->on('categories');
+                    $table->timestamps();
+                });
     }
 
     /**

eb325db43bb89e3d3330c69548302d20b2e317a0
Med Faji <meedfaji@gmail.com>
2023-10-20 15:54:19 +0100

    Updating routes

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
-Route::resource('categories', CategoryController::class);
+// Products routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{productId}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{productId}', [ProductController::class, 'update']);
+Route::delete('/products/{productId}', [ProductController::class, 'destroy']);
+
+// Categories routes
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{categoryId}', [CategoryController::class, 'show']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{categoryId}', [CategoryController::class, 'update']);
+Route::delete('/categories/{categoryId}', [CategoryController::class, 'destroy']);
+
